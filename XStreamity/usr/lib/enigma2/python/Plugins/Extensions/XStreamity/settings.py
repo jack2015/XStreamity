4f9646b59b053c552c4e1f1ea81adc93db2f6387
Redouane <redouane343@yahoo.com>
2020-06-20 17:11:23 +0200

    Update settings.py

--- XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/settings.py
+++ XStreamity/usr/lib/enigma2/python/Plugins/Extensions/XStreamity/settings.py
@@ -14,6 +14,7 @@ from Screens.MessageBox import MessageBox
 from Screens.ParentalControlSetup import ProtectedScreen
 from Screens.Screen import Screen
 from xStaticText import StaticText
+from enigma import getBoxBrand
 
 import xstreamity_globals as glob
 import os
@@ -29,8 +30,11 @@ class XStreamity_Settings(ConfigListScreen, Screen, ProtectedScreen):
 		self.session = session
 		
 		skin = skin_path + 'settings.xml'
+		try:
+			from boxbranding import getImageDistro, getImageVersion, getOEVersion
+		except:
 		
-		if os.path.exists('/var/lib/dpkg/status'):
+		if getBoxBrand() == "dreambox":
 			skin = skin_path + 'DreamOS/settings.xml'
 	
 		with open(skin, 'r') as f:
